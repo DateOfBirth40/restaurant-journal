1cf0c24794bda140d6244db6b4a00983f39f5ff2
Derek O'Brien <derekobrien40@gmail.com>
2023-09-13 19:22:59 -0400

    Create non-functional modal components

---
+++ src/client/components/log-in-modal.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { Form, Modal, Button, Row } from 'react-bootstrap'
+
+function LogInModal(props) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">Log In</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Row>
+            <Form.Group controlId="formUsername" className="mb-3">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+            <Form.Group controlId="formPassword" className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" />
+            </Form.Group>
+          </Row>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button>Log In</Button>
+        <Button onClick={props.onHide} variant="secondary">Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default LogInModal;
